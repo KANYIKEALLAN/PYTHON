0f3d45875dd7d33dca3d010f1d50672cc6236522
ALLAN <157680781+KANYIKEALLAN@users.noreply.github.com>
2024-01-31 23:03:06 +0300

    Create variable_input.py
    variable

---
+++ variable_input.py
@@ -0,0 +1,10 @@
+student_name=input("Enter Name:")
+
+course_work_mark=int(input("Enter Course Work Marks:"))
+
+exam_score=input("Enter Exam Score:")
+
+score_conversion=(course_work_mark/100)*70
+
+
+print(score_conversion)

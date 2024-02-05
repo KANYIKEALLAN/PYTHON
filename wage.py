3ce1440993de526ba8d1adcb9f5893da0a3c2c8a
ALLAN <157680781+KANYIKEALLAN@users.noreply.github.com>
2024-02-05 14:02:33 +0300

    Update wage.py

--- wage.py
+++ wage.py
@@ -5,12 +5,12 @@ RATE=30000
 
 
 wage=hours_worked * RATE
-allaowance=int(0.05 * wage)
-gross_wage=allaowance + wage
+allowance=int(0.05 * wage)
+gross_wage=allowance + wage
 tax=int(0.05 * gross_wage)
 net_wage=gross_wage - tax
 
-print(f"\nWage:{wage}\n\nAllowance:{allaowance}\n\nGross Wage:{wage}\n\nTax:{tax}\n\nNet Wage:{net_wage}\n\n")
+print(f"\nWage:{wage}\n\nAllowance:{allowance}\n\nGross Wage:{wage}\n\nTax:{tax}\n\nNet Wage:{net_wage}\n\n")
 
 
 
